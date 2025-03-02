05499cbf59063d0663cbaeb57af19f449fd04223
Arabito18 <ajferreras6@yahoo.com>
2025-03-02 09:32:12 -0400

    Fivem Tunning Sytem

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -64,23 +64,18 @@ files {
 
     'html/*.svg',
 
-	"carcols_gen9.meta",
+	
+	
 
-	"carmodcols_gen9.meta",
+	
 
-	"carmodcols.ymt",
-
-	"stream/vehicle_paint_ramps.ytd"
+	
 
 }
 
 
 
-data_file "CARCOLS_GEN9_FILE" "carcols_gen9.meta"
 
-data_file "CARMODCOLS_GEN9_FILE" "carmodcols_gen9.meta"
-
-data_file "FIVEM_LOVES_YOU_447B37BE29496FA0" "carmodcols.ymt"
 
 
 
